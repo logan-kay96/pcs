9516a2063b0528e57fcedeede8f65daaba974804
Catguythe3rd <mathieuandlebaron@gmail.com>
2024-08-21 11:35:26 -0600

    Reset page to basic.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. horse
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,9 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-
-      <body> test
-      </body>
     </div>
   );
 }
